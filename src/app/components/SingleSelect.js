6d83b2b226743689109d80e62df07f565545cd6c
Bryce <br.kalow@gmail.com>
2015-08-21 13:46:22 -0500

    Added SingleSelect component

--- src/app/components/SingleSelect.js
+++ src/app/components/SingleSelect.js
@@ -6,6 +6,16 @@ import React from 'react/addons';
  */
 class SingleSelect extends React.Component {
 
+  constructor(props) {
+    super(props);
+    this.state = { selectValue: '' };
+  }
+
+  onSelect(event) {
+    this.setState({selectValue: event.target.value}, this.props.selectHandler);
+    // this.props.selectHandler();
+  }
+
   /*
    * @method render
    * @returns {JSX}
@@ -13,8 +23,12 @@ class SingleSelect extends React.Component {
   render () {
     var options = this.props.collection.map(function(item) {
       return <option value={ item[this.props.selectValue] }>{ item[this.props.selectLabel] }</option>
-    })
-    return <select>{ options }</select>
+    }.bind(this))
+    return(
+    <select value={this.state.selectValue} onChange={this.onSelect.bind(this)}>
+      <option value="" selected="true">Select an option</option>
+      { options }
+    </select>);
   }
 }
 
