8a0a0ceaff8a46e064ecc2a2ac5c4cc490c6d965
Bryce <br.kalow@gmail.com>
2015-08-21 13:46:43 -0500

    Added SingleSelects and form visiblity toggle

--- src/app/components/SearchForm.js
+++ src/app/components/SearchForm.js
@@ -11,7 +11,7 @@ class SearchForm extends React.Component {
 
   constructor(props) {
     super(props);
-    this.state = { campuses: [], terms: [] };
+    this.state = { campuses: [], terms: [], formVisible: false };
   }
 
   /*
@@ -23,8 +23,8 @@ class SearchForm extends React.Component {
   }
 
   componentWillMount() {
-    this.fetch('campuses.json').then(function(response) { this.state.campuses = response.data.campuses; });
-    this.fetch('terms.json').then(function(response) { this.state.terms = response.data.terms; });
+    this.fetch('campuses.json').then(function(response) { this.setState({"campuses": response.campuses}); }.bind(this));
+    this.fetch('terms.json').then(function(response) { this.setState({"terms": response.terms}); }.bind(this));
   }
 
   fetch(route) {
@@ -32,10 +32,21 @@ class SearchForm extends React.Component {
   }
 
   decodeTerm(sTerm) {
-    var year = sTerm.slice(1,3);
+    var year = sTerm.substring(1,3);
     var terms = { "3": "Spring", "5": "Summer", "9": "Fall" };
     var term = terms[sTerm[3]];
-    return `${ term } 20${ year }` ;
+    return `${ term } 20${ year }`;
+  }
+
+  revealFormHandler() {
+    var campus = this.refs.campusSelect.state.selectValue;
+    var term = this.refs.termSelect.state.selectValue;
+
+    if (campus != "" && term != "") {
+      this.setState({formVisible: true});
+    } else {
+      this.setState({formVisible: false});
+    }
   }
 
   executeSearch() {}
@@ -45,16 +56,29 @@ class SearchForm extends React.Component {
    * @returns {JSX}
    */
   render () {
+    var fullForm;
+
+    if (this.state.formVisible) {
+      fullForm = (
+        <p>See me</p>
+      );
+    }
+
+    var terms = this.state.terms.map(function(term) {
+      return {"sterm": term.strm, "label": this.decodeTerm(term.strm)};
+    }.bind(this))
+
     return <div className="searchform">
       <form onSubmit={ this.executeSearch() }>
         <div className="form-inline">
           <div className="form-group">
-            <SingleSelect collection={ this.state.campuses } selectHandler={ function(){} } selectLabel="campus" selectValue="campus"/>
+            <SingleSelect ref="campusSelect" collection={ this.state.campuses } selectHandler={this.revealFormHandler.bind(this)} selectLabel="abbreviation" selectValue="abbreviation"/>
           </div>
           <div className="form-group">
-
+            <SingleSelect ref="termSelect" collection={ terms } selectHandler={this.revealFormHandler.bind(this)} selectLabel="label" selectValue="sterm" />
           </div>
         </div>
+        {fullForm}
       </form>
     </div>;
   }
