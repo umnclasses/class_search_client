2c2e3893fbf2e335e94eae75250af2c647f4335d
Bryce <br.kalow@gmail.com>
2015-08-21 13:46:06 -0500

    Don't use https with api

--- config/app.js
+++ config/app.js
@@ -1,6 +1,6 @@
 var config = {};
 
 config.title = 'UMN Course Search';
-config.apiRoot = 'https://courses-staging.umn.edu/';
+config.apiRoot = 'http://courses-staging.umn.edu/';
 
 module.exports = config;
