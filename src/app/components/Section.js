b593626409be7bebbbd61131dc7b3f80826f8b19
Max Fierke <max@maxfierke.com>
2015-08-21 23:14:26 -0500

    Make enrollment status and counts pretty with bootstrap labels
    Signed-off-by: Max Fierke <max@maxfierke.com>

--- src/app/components/Section.js
+++ src/app/components/Section.js
@@ -11,6 +11,20 @@ class Section extends React.Component {
     return React.addons.PureRenderMixin.shouldComponentUpdate.apply(this, arguments);
   }
 
+  renderStatusLabel(status) {
+    var labelClasses = {
+      open: 'label label-success text-capitalize',
+      closed: 'label label-danger text-capitalize',
+      waitlist: 'label label-warning text-capitalize'
+    };
+
+    return (
+      <span className={labelClasses[status]}>
+        {status}
+      </span>
+    );
+  }
+
   render() {
     var meetingPatterns = this.props.section.meeting_patterns.map(function(meetingPattern, index) {
       return (
@@ -26,10 +40,12 @@ class Section extends React.Component {
           <span className="glyphicon glyphicon-chevron-right pull-right arrow" aria-hidden="true"></span>
           <dl>
             <dt>Status:</dt>
-            {this.props.section.enrollment.status}
-            <span className="badge">
-              {this.props.section.enrollment.enrollment_total}/{this.props.section.enrollment.enrollment_capacity}
-            </span>
+            <div className="label-group h4">
+              {this.renderStatusLabel(this.props.section.enrollment.status)}
+              <span className="label label-default">
+                {this.props.section.enrollment.enrollment_total}/{this.props.section.enrollment.enrollment_capacity}
+              </span>
+            </div>
             <dt>Meets:</dt>
             {meetingPatterns}
             <dt>Session:</dt>
