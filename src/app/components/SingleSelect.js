394a3cc91a6ad1f0018c1bb3407c560d41956b63
Bryce <br.kalow@gmail.com>
2015-08-21 21:56:58 -0500

    Added key to each collection component, used select value prop instead of specifying selected on default option

--- src/app/components/SingleSelect.js
+++ src/app/components/SingleSelect.js
@@ -22,13 +22,21 @@ class SingleSelect extends React.Component {
    */
   render () {
     var options = this.props.collection.map(function(item) {
-      return <option value={ item[this.props.selectValue] }>{ item[this.props.selectLabel] }</option>
+      return(
+        <option key={item[this.props.selectValue]} value={item[this.props.selectValue]}>
+          {item[this.props.selectLabel]}
+        </option>
+      );
     }.bind(this))
     return(
-    <select value={this.state.selectValue} onChange={this.onSelect.bind(this)}>
-      <option value="" selected="true">Select an option</option>
-      { options }
-    </select>);
+      <div className="form-group">
+        <label>{this.props.label}</label>
+        <select value={this.state.selectValue} onChange={this.onSelect.bind(this)} className="form-control">
+          <option value="">Select an option</option>
+          { options }
+        </select>
+      </div>
+    );
   }
 }
 
@@ -37,8 +45,7 @@ SingleSelect.propTypes = {
   collection: React.PropTypes.array.isRequired,
   selectHandler: React.PropTypes.func.isRequired,
   selectLabel: React.PropTypes.string.isRequired,
-  selectValue: React.PropTypes.string.isRequired,
-  selectId: React.PropTypes.string.isRequred
+  selectValue: React.PropTypes.string.isRequired
 };
 
 export default SingleSelect;
