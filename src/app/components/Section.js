26a9798b84a6a72a3febce355297fe7cef2226d5
Bryce <br.kalow@gmail.com>
2015-08-21 21:54:44 -0500

    Fixed up meetingPatterns rendering and added course identifier

--- src/app/components/Section.js
+++ src/app/components/Section.js
@@ -12,22 +12,27 @@ class Section extends React.Component {
   }
 
   render() {
-    var meetingPatterns = this.props.section.meeting_patterns.map(function(meetingPattern) {
+    var meetingPatterns = this.props.section.meeting_patterns.map(function(meetingPattern, index) {
       return (
-        <MeetingPattern meetingPattern={meetingPattern} />
-      )
+        <MeetingPattern key={index} meetingPattern={meetingPattern} />
+      );
     });
 
     return (
         <li className="list-group-item">
           <h4>
-              Section {this.props.section.number} - {this.props.section.type}
+              Section {this.props.section.number} - {this.props.section.component}
           </h4>
           <span className="glyphicon glyphicon-chevron-right pull-right arrow" aria-hidden="true"></span>
           <dl>
-            <dt>Meets</dt>
+            <dt>Status:</dt>
+            {this.props.section.enrollment.status}
+            <span className="badge">
+              {this.props.section.enrollment.enrollment_total}/{this.props.section.enrollment.enrollment_capacity}
+            </span>
+            <dt>Meets:</dt>
             {meetingPatterns}
-            <dt>Session</dt>
+            <dt>Session:</dt>
             <dd>001 Regular Academic Session</dd>
           </dl>
         </li>
